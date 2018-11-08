f3b1d4294ba984abf595820824dba3bce70ebb64
evhollar <33362975+evhollar@users.noreply.github.com>
2018-11-08 17:16:15 -0500

    Added new Blokus class

---
+++ blokus/src/Blokus.java
@@ -0,0 +1,149 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+/**
+ * The blokus Application, which contains the board and a message label.
+ *
+ * @author Evan Hollar
+ */
+public class Blokus extends Application {
+
+    public static double MILLISEC = 300;
+    private Game game;
+    private Board board;
+    private Timeline animation;
+    private Label statusLabel;
+
+    /**
+     * Launches the application.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    /**
+     * Sets up the blokus board and game, as well as a status label that can be
+     * used to display scores and messages.
+     *
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        board = new Board();
+
+        statusLabel = new Label("Tetris");
+        statusLabel.setTextFill(Color.BLACK);
+
+        BorderPane pane = new BorderPane();
+        pane.setCenter(board);
+        pane.setTop(statusLabel);
+
+        Scene scene = new Scene(pane);
+
+        game = new Game(this, board);
+
+        // Can add events that occur automatically here.
+        // setUpAnimation();
+        setUpKeyPresses();
+
+        setUpMouseEvents();
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+    /**
+     * Changes the message in the status label at the top of the screen.
+     *
+     * @param message
+     */
+    public void setMessage(String message) {
+        statusLabel.setText(message);
+    }
+
+    /**
+     * Sets up an animation timeline that calls update on the game every
+     * MILLISEC milliseconds.
+     */
+//    private void setUpAnimation() {
+//        // Create a handler
+//        EventHandler<ActionEvent> eventHandler = (ActionEvent e) -> {
+//            this.pause();
+//            game.update();
+//            this.resume();
+//        };
+//        // Create an animation for alternating text
+//        animation = new Timeline(new KeyFrame(Duration.millis(MILLISEC), eventHandler));
+//        animation.setCycleCount(Timeline.INDEFINITE);
+//        animation.play();
+//    }
+    /**
+     * Sets up key events for the arrow keys and space bar. All keys send
+     * messages to the game, which should react appropriately.
+     */
+    private void setUpKeyPresses() {
+        board.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case LEFT:
+                    game.rotateLeft();
+                    break;
+                case RIGHT:
+                    game.rotateRight();
+                    break;
+                case DOWN:
+                    break;
+                case UP:
+                    break;
+                case M:
+                    game.mirror();
+                    break;
+                case SPACE:
+                    break;
+
+            }
+        });
+        board.requestFocus(); // board is focused to receive key input
+
+    }
+
+    /**
+     * Handles the mouse click.
+     */
+    private void setUpMouseEvents() {
+        board.setOnMousePressed(e -> {
+            game.placePiece();
+        });
+        board.setOnMouseMoved(e -> {
+        game.hover(e.getSceneX(), e.getSceneY());
+        });
+    }
+
+    /**
+     * Pauses the animation.
+     */
+    private void pause() {
+        animation.pause();
+    }
+
+    /**
+     * Resumes the animation.
+     */
+    private void resume() {
+        animation.play();
+    }
+
+}

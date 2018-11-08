bba8a7f7ff7f677dae60eebf4b96eb36a30b38b6
evhollar <33362975+evhollar@users.noreply.github.com>
2018-11-08 17:13:03 -0500

    Changed Everything

--- blokus/src/Game.java
+++ blokus/src/Game.java
@@ -1,22 +1,91 @@
+
 import java.util.ArrayList;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import java.util.Arrays;
 import javafx.scene.paint.Color;
 
-public class Game extends Application{
-    
-    @Override
-    public void start(Stage primaryStage) {
-        Board board = new Board();
-        
-        Piece uBlock = new Piece(17, 10, 10, board); //in theory, places a u-block at the center of the board
+/**
+ * BlokusGame class
+ *
+ * @author Evan Hollar
+ */
+public class Game {
+
+    private final Blokus blokusApp;
+    private Piece activePiece;
+    private Board board;
+    private int piece = 0;
+    private final Color[] Colors = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW};
+    private int turn = 0;
+
+    /**
+     * Initialize the game. Selects a random shape to act as the current piece.
+     *
+     * @paramblokusApp A reference to the application (use to set messages).
+     * @param board A reference to the board on which blocks are drawn
+     */
+    public Game(Blokus blokusApp, Board board) {
+
+        //Initializes the game with the piece.
+        activePiece = new Piece(board, Colors[turn], Board.DIM_SQUARES / 2, 2, piece);
+
+        this.board = board;
+
+        this.blokusApp = blokusApp;
+        blokusApp.setMessage("BLOKUS");
     }
-        public static void main(String[] args) {
-            launch(args);
+
+    /**
+     * Anything that happens automatically can go here.
+     */
+    void update() {
+    }
+
+    /**
+     * Rotate the current piece counter-clockwise.
+     */
+    void rotateLeft() {
+        this.activePiece.rotate(0);
+        System.out.println("rotate left key was pressed!");
+    }
+
+    /**
+     * Rotate the current piece clockwise.
+     */
+    void rotateRight() {
+        this.activePiece.rotate(1);
+        System.out.println("rotate right key was pressed!");
+    }
+
+    /**
+     * Mirror the current piece.
+     */
+    void mirror() {
+        this.activePiece.rotate(2);
+        System.out.println("mirror key was pressed!");
+    }
+
+    /**
+     * Handle the mouse clicked event.
+     */
+    void click(double x, double y) {
+    }
+
+    void hover(double x, double y) {
+        this.activePiece.move((int) x / Board.SQUARE_SIZE, (int) (y - 20) / Board.SQUARE_SIZE);
+    }
+
+    /**
+     * Place the piece onto the board.
+     */
+    void placePiece() {
+        if(this.activePiece.addPieceToBoard()){
+        turn++;
+        piece = (int) (Math.random() * 21);
+        if (turn > 3) {
+            turn = 0;
+        }
+        activePiece = new Piece(board, Colors[turn], Board.DIM_SQUARES / 2, 2, piece);
+        System.out.println("Space was pressed");
+        }
     }
 }

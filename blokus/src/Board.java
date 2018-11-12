9347341be7d91caeb1a19d7bba78aebda4bd1e2d
evhollar <33362975+evhollar@users.noreply.github.com>
2018-11-12 03:26:31 -0500

    Added Lines and the starting dots

--- blokus/src/Board.java
+++ blokus/src/Board.java
@@ -1,10 +1,13 @@
 
+import java.util.ArrayList;
 import javafx.geometry.Insets;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
 /**
  * A Pane in which blokus squares can be displayed.
@@ -16,9 +19,12 @@ public class Board extends Pane {
     public static final int BLOCK_SIZE = 20;
     // The number of squares that fit on the screen in the x and y dimensions
     public static final int DIM_SQUARES = 20;
+    private final ArrayList<Line> horizontalLines = new ArrayList<>();
+    private final ArrayList<Line> verticalLines = new ArrayList<>();
+    private final ArrayList<Circle> startingCircles = new ArrayList<>();
     public int score;
-    
-        //Creates a 2D array that will track the locations of all the pieces on the board.
+
+    //Creates a 2D array that will track the locations of all the pieces on the board.
     public Block[][] squares = new Block[DIM_SQUARES][DIM_SQUARES];
 
     /**
@@ -28,6 +34,27 @@ public class Board extends Pane {
     public Board() {
         this.setPrefHeight(DIM_SQUARES * BLOCK_SIZE);
         this.setPrefWidth(DIM_SQUARES * BLOCK_SIZE);
+
+        //Draw the grid on the board
+        for (int i = 0; i < DIM_SQUARES - 1; i++) {
+            horizontalLines.add(new Line(0, (i + 1) * BLOCK_SIZE, DIM_SQUARES * BLOCK_SIZE, (i + 1) * BLOCK_SIZE));
+            verticalLines.add(new Line((i + 1) * BLOCK_SIZE, 0, (i + 1) * BLOCK_SIZE, DIM_SQUARES * BLOCK_SIZE));
+        }
+        //Create the starting circles
+        startingCircles.add(new Circle(BLOCK_SIZE/2, BLOCK_SIZE/2, BLOCK_SIZE/2, Color.RED));
+        startingCircles.add(new Circle(BLOCK_SIZE/2, DIM_SQUARES*(BLOCK_SIZE-.5), BLOCK_SIZE/2, Color.BLUE));
+        startingCircles.add(new Circle(DIM_SQUARES*(BLOCK_SIZE-.5), BLOCK_SIZE/2, BLOCK_SIZE/2, Color.GREEN));
+        startingCircles.add(new Circle(DIM_SQUARES*(BLOCK_SIZE-.5), DIM_SQUARES*(BLOCK_SIZE-.5), BLOCK_SIZE/2, Color.YELLOW));
+        
+        for (int i = 0; i < 4; i++) {
+            this.getChildren().add(startingCircles.get(i));
+        }
+
+        for (int i = 0; i < horizontalLines.size(); i++) {
+            this.getChildren().add(horizontalLines.get(i));
+            this.getChildren().add(verticalLines.get(i));
+        }
+
         BackgroundFill myBF = new BackgroundFill(Color.WHITE, new CornerRadii(1),
                 new Insets(0.0, 0.0, 0.0, 0.0));// or null for the padding
         setBackground(new Background(myBF));

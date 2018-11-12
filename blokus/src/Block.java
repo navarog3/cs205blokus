09ef65295e8964af35a54dce12de0d2c945d2c4a
evhollar <33362975+evhollar@users.noreply.github.com>
2018-11-12 01:32:03 -0500

    Update Block.java
    Added a color field

--- blokus/src/Block.java
+++ blokus/src/Block.java
@@ -11,8 +11,8 @@ import javafx.scene.shape.Line;
 /**
  * A single Square on the blokus board. Squares have fixed height and width and
  * are positioned at grid locations on the board. A square at board location 3,
- * 4 for example, would be drawn at 3*TetrisBoard.SQUARE_SIZE,
- * 4*TetrisBoard.SQUARE_SIZE.
+ * 4 for example, would be drawn at 3*TetrisBoard.BLOCK_SIZE,
+ * 4*TetrisBoard.BLOCK_SIZE.
  *
  * The blokus board x and y locations of the Square are IntegerProperties.
  */
@@ -26,29 +26,31 @@ public class Block {
     // The y location in board coordinates
     private int yPos;
     public final Board board;
+    private Color color;
 
     /**
      * Creates a block and draws it in the board. The shape for a block is a
-     * rectangle with height and width equal to Board.SQUARE_SIZE. The location
-     * of the shape is set to blokus_x*TetrisBoard.SQUARE_SIZE,
-     * blokus_y*TetrisBoard.SQUARE_SIZE via bindings, so that whenever blokus_x
+     * rectangle with height and width equal to Board.BLOCK_SIZE. The location
+     * of the shape is set to blokus_x*TetrisBoard.BLOCK_SIZE,
+     * blokus_y*TetrisBoard.BLOCK_SIZE via bindings, so that whenever blokus_x
      * and blokus_y are updated, the block's location will update.
      *
      * @param board
      */
-    public Block(Board board) {
+    public Block(Board board, Color color) {
         this.board = board;
+        this.setColor(color);
         this.board.getChildren().add(shape);
         for (int i = 0; i < 4; i++) {
             this.board.getChildren().add(borders[i]);
         }
-        this.shape.setWidth(Board.SQUARE_SIZE);
-        this.shape.setHeight(Board.SQUARE_SIZE);
+        this.shape.setWidth(Board.BLOCK_SIZE);
+        this.shape.setHeight(Board.BLOCK_SIZE);
 
         // set the x and y locations so that they are always a multiple
         // of the size of a grid square
-        // shape.xProperty().bind(blokus_x.multiply(board.SQUARE_SIZE));
-        // shape.yProperty().bind(blokus_y.multiply(board.SQUARE_SIZE));
+        // shape.xProperty().bind(blokus_x.multiply(board.BLOCK_SIZE));
+        // shape.yProperty().bind(blokus_y.multiply(board.BLOCK_SIZE));
     }
 
     /**
@@ -60,29 +62,29 @@ public class Block {
      * @param y y-coordinate on the blokus board
      */
     public void moveToBlokusLocation(int x, int y) {
-        this.shape.setX(x * Board.SQUARE_SIZE);
-        this.shape.setY(y * Board.SQUARE_SIZE);
+        this.shape.setX(x * Board.BLOCK_SIZE);
+        this.shape.setY(y * Board.BLOCK_SIZE);
         //Top border
-        this.borders[0].setStartX(x * Board.SQUARE_SIZE);
-        this.borders[0].setEndX((x + 1) * Board.SQUARE_SIZE);
-        this.borders[0].setStartY(y * Board.SQUARE_SIZE);
-        this.borders[0].setEndY(y * Board.SQUARE_SIZE);
-        //Left Border
-        this.borders[1].setStartX(x * Board.SQUARE_SIZE);
-        this.borders[1].setEndX(x * Board.SQUARE_SIZE);
-        this.borders[1].setStartY(y * Board.SQUARE_SIZE);
-        this.borders[1].setEndY((y + 1) * Board.SQUARE_SIZE);
-//        //Right Border
-//        this.borders[2].setStartX((x + 1) * Board.SQUARE_SIZE);
-//        this.borders[2].setEndX((x + 1) * Board.SQUARE_SIZE);
-//        this.borders[2].setStartY(y * Board.SQUARE_SIZE);
-//        this.borders[2].setEndY((y + 1) * Board.SQUARE_SIZE);
-//        //Bottom Border
-//        this.borders[2].setStartX(x * Board.SQUARE_SIZE);
-//        this.borders[2].setEndX((x + 1) * Board.SQUARE_SIZE);
-//        this.borders[2].setStartY((y + 1) * Board.SQUARE_SIZE);
-//        this.borders[2].setEndY((y + 1) * Board.SQUARE_SIZE);
-        
+//        this.borders[0].setStartX(x * Board.BLOCK_SIZE);
+//        this.borders[0].setEndX((x + 1) * Board.BLOCK_SIZE);
+//        this.borders[0].setStartY(y * Board.BLOCK_SIZE);
+//        this.borders[0].setEndY(y * Board.BLOCK_SIZE);
+//        //Left Border
+//        this.borders[1].setStartX(x * Board.BLOCK_SIZE);
+//        this.borders[1].setEndX(x * Board.BLOCK_SIZE);
+//        this.borders[1].setStartY(y * Board.BLOCK_SIZE);
+//        this.borders[1].setEndY((y + 1) * Board.BLOCK_SIZE);
+        //Right Border
+        this.borders[2].setStartX((x + 1) * Board.BLOCK_SIZE);
+        this.borders[2].setEndX((x + 1) * Board.BLOCK_SIZE);
+        this.borders[2].setStartY(y * Board.BLOCK_SIZE);
+        this.borders[2].setEndY((y + 1) * Board.BLOCK_SIZE);
+        //Bottom Border
+        this.borders[3].setStartX(x * Board.BLOCK_SIZE);
+        this.borders[3].setEndX((x + 1) * Board.BLOCK_SIZE);
+        this.borders[3].setStartY((y + 1) * Board.BLOCK_SIZE);
+        this.borders[3].setEndY((y + 1) * Board.BLOCK_SIZE);
+
         this.xPos = x;
         this.yPos = y;
     }
@@ -112,6 +114,16 @@ public class Block {
      */
     public void setColor(Color color) {
         shape.setFill(color);
+        this.color = color;
+    }
+
+    /**
+     * Sets the color of the square.
+     *
+     * @param color
+     */
+    public Color getColor() {
+        return this.color;
     }
 
     /**
